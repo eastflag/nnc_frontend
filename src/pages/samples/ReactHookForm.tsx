6a5113b50c226578e372cf26c499d4271acfbef4
A75178 <eastflag@cnspartner.com>
2024-06-28 16:08:10 +0900

    react hook form 수정

--- src/pages/samples/ReactHookForm.tsx
+++ src/pages/samples/ReactHookForm.tsx
@@ -2,28 +2,34 @@ import {SubmitHandler, useForm} from "react-hook-form";
 import styles from "./ReactHookForm.module.css"
 
 type Inputs = {
-    example: string,
-    exampleRequired: string,
+  name: string,
+  password: string,
 };
 
 export const ReactHookForm = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
-    const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+  const {register, handleSubmit, watch, formState: {errors}} = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
 
-    console.log(watch("example")) // watch input value by passing the name of it
+  console.log(watch("name")) // watch input value by passing the name of it
 
-    return (
-        /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-            {/* register your input into the hook by invoking the "register" function */}
-            <input className={styles.input} defaultValue="test" {...register("example")} />
+  return (
+    <>
+      <h3>Form Validation Sample: react-hook-form</h3>
 
-            {/* include validation with required or other standard HTML validation rules */}
-            <input className={styles.input} {...register("exampleRequired", { required: true })} />
-            {/* errors will return when field validation fails  */}
-            {errors.exampleRequired && <span>This field is required</span>}
+      {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <input className={styles.input} defaultValue="" {...register("name", {required: true})} />
+        {/* errors will return when field validation fails  */}
+        {errors.name && <span>name is required</span>}
 
-            <button className={styles.submit} type="submit">제출</button>
-        </form>
-    );
+        {/* include validation with required or other standard HTML validation rules */}
+        <input className={styles.input} {...register("password", {required: true})} />
+        {/* errors will return when field validation fails  */}
+        {errors.password && <span>password is required</span>}
+
+        <button className={styles.submit} type="submit">제출</button>
+      </form>
+    </>
+  );
 }
