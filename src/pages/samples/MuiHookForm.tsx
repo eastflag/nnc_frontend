fa3f6e64be611a43e2b0146527975db27aa5863c
A75178 <eastflag@cnspartner.com>
2024-08-08 18:03:48 +0900

    .

--- src/pages/samples/MuiHookForm.tsx
+++ src/pages/samples/MuiHookForm.tsx
@@ -1,15 +1,16 @@
 import {useForm} from "react-hook-form";
 import {Input} from "../../components/Input.tsx";
+import {Box, Button} from "@mui/material";
 
 interface FormValue {
-  name?: string;
+  email?: string;
   password?: string;
 }
 
 export const MuiHookForm = () => {
   const { control, handleSubmit } = useForm<FormValue>({
     defaultValues: {
-      name: "",
+      email: "",
       password: ""
     },
   });
@@ -23,29 +24,45 @@ export const MuiHookForm = () => {
       <h3>MUI Form Validation Sample: MUI + react-hook-form</h3>
 
       {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <Box component="form" onSubmit={handleSubmit(onSubmit)}>
         <Input
-          name="name"
+          name="email"
           control={control}
-          rules={{ required: "필수!" }}
+          rules={{
+            required: "Email is required",
+            minLength: {value: 3, message: "Email must be at least 3 characters long"},
+            maxLength: {value: 20, message: "Email must be less than 20 characters long"},
+            pattern: {value: /^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$/, message: "Invalid email address"}
+          }}
           textFieldProps={{
             label: "Email Address",
             fullWidth: true,
-            margin: "normal"
+            margin: "normal",
+            size: "small"
           }}
         />
         <Input
           name="password"
           control={control}
-          rules={{ required: "필수!" }}
+          rules={{ /*required: "Password is required",*/
+            // custom validation:
+            // 예: password 입력란이 두개가 있고 두번째 password는 두번째 password와 동일한지 체크할 경우 이것을 이용
+            validate: (value: any, formValues: any) => {
+              console.log(`value: ${value}, formValues: ${formValues.email}`);
+              return formValues.email === "aaa@gmail.com" || "password error message";
+            },
+
+          }}
           textFieldProps={{
             label: "Password",
             fullWidth: true,
-            margin: "normal"
+            margin: "normal",
+            size: "small"
           }}
         />
-        <button type="submit">submit</button>
-      </form>
+        <Button type="submit" fullWidth variant="contained"
+                sx={{ mt: 2 }}>submit</Button>
+      </Box>
     </>
   );
 }
