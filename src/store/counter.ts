f9b6a4b834bf80d122afb136d5eef3787af8df42
A75178 <eastflag@cnspartner.com>
2024-08-26 16:04:53 +0900

    .

--- src/store/counter.ts
+++ src/store/counter.ts
@@ -26,4 +26,4 @@ export const counterSlice = createSlice({
 });
 
 export const counterActions = counterSlice.actions;
-export default counterSlice.reducer;
+export default counterSlice;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './counter';
+import counterSlice from './counter';
 
 const store = configureStore({
   reducer: {
-    count: counterReducer,
+    count: counterSlice.reducer,
   },
 });
 
