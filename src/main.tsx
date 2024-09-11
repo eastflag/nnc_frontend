988bf5f72d7a61b9b75629ccb20876fa7ed48608
DK <eastflag@cnspartner.com>
2024-09-11 09:27:42 +0900

    roboto 폰트 설정

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,11 @@ import {createTheme, ThemeProvider} from "@mui/material";
 import {Provider} from "react-redux";
 import store from "./store";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 const theme = createTheme({
   palette: {
     background: {
