09a8ea7521d6558e932d9b3483dea28a2d2cf9b8
A75178 <eastflag@cnspartner.com>
2024-11-06 18:30:56 +0900

    .

--- src/pages/admin/contents/News.tsx
+++ src/pages/admin/contents/News.tsx
@@ -1,18 +1,26 @@
-import {useEffect, useMemo, useRef, useState} from "react";
+import {ChangeEvent, useEffect, useMemo, useRef, useState} from "react";
 import customAxios from "../../../utils/customAxios.ts";
 import {
   Box,
   Button,
   FormControl,
   InputLabel,
-  MenuItem,
+  MenuItem, Pagination,
   Select,
-  SelectChangeEvent,
+  SelectChangeEvent, Stack,
   Typography
 } from "@mui/material";
 import {useForm} from "react-hook-form";
 import {Input} from "../../../components/Input.tsx";
 import ReactQuill from "react-quill";
+import {
+  DataGrid,
+  GridColDef,
+  GridEventListener,
+  GridRowEditStopReasons,
+  GridRowModel,
+  GridRowModesModel
+} from "@mui/x-data-grid";
 
 interface Category {
   id: number;
@@ -25,7 +33,17 @@ interface FormValue {
 }
 
 function News() {
-  // new category
+  // grid
+  const [newsList, setNewsList] = useState([]);
+  const [rowModesModel, setRowModesModel] = useState<GridRowModesModel>({});
+  // 검색
+  const [title, setTitle] = useState('');
+  // pagination
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(5);
+  const [total, setTotal] = useState<number>(0);
+  const [count, setCount] = useState<number>(0);
+  // news category
   const [categoryList, setCategoryList] = useState<Category[]>([]);
   const [category, setCategory] = useState<string>('');
   // new title, content
@@ -56,7 +74,72 @@ function News() {
 
   useEffect(() => {
     getCategoryList();
+    getNewsList();
   }, []);
+
+  // grid start --------------------------------------------
+  const columns: GridColDef[] = [
+    {
+      field: 'id',
+      headerName: 'ID',
+      width: 100,
+      // hide: true,
+    },
+    {
+      field: 'title',
+      headerName: 'e-mail',
+      width: 200,
+    },
+    {
+      field: 'created',
+      headerName: 'created date',
+      width: 200,
+    },
+    {
+      field: 'updated',
+      headerName: 'updated date',
+      width: 200,
+    },
+  ]
+
+  const handleRowModesModelChange = (newRowModesModel: GridRowModesModel) => {
+    console.log('handleRowModesModelChange', newRowModesModel);
+    setRowModesModel(newRowModesModel);
+  };
+
+  const  handleRowEditStop: GridEventListener<'rowEditStop'> = (params, event) => {
+    console.log('handleRowEditStop');
+    if (params.reason === GridRowEditStopReasons.rowFocusOut) {
+      event.defaultMuiPrevented = true;
+    }
+  };
+
+  const processRowUpdate = async (newRow: GridRowModel, oldRow: GridRowModel) => {
+    // add or update
+    console.log('processRowUpdate: ', newRow);
+    const updatedRow = { ...newRow, isNew: false };
+    // setUsers(users.map((manage: any) => (manage.id === newRow.id ? updatedRow : manage)));
+
+    try {
+      const response = await customAxios.put("/api/v1/admin/user", newRow);
+      console.log(response);
+      return updatedRow;
+    } catch(e: any) {
+      return oldRow;
+    }
+  };
+  // grid end --------------------------------------------
+
+  // pagination start ------------------------------------
+  const handleSize = (event: any) => {
+    console.log(event);
+    setPageSize(event.target.value)
+  };
+
+  const handleChangePage = (event: ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
+  // pagination end --------------------------------------
 
   function imageHandler(){
     const editor = quillRef?.current.getEditor();
@@ -80,6 +163,24 @@ function News() {
         console.log('You could only upload images.');
       }
     };
+  }
+
+  const getNewsList = async () => {
+    const params = {
+      title: title,
+      page: page-1,
+      size: pageSize,
+    }
+    try {
+      const response = await customAxios.get('/api/v1/manager/board/paged_list',
+        {params});
+      console.log(response);
+      setNewsList(response.data.content);
+      setTotal(response.data.totalElements);
+      setCount(response.data.totalPages);
+    } catch(e) {
+      console.log(e);
+    }
   }
 
   const getCategoryList = async () => {
@@ -126,6 +227,43 @@ function News() {
           }
         </Select>
       </FormControl>
+
+      <Box sx={{
+        borderBottom: 1,
+        borderColor: 'divider'
+      }}>
+        <DataGrid
+          columns={columns}
+          rows={newsList}
+          editMode="row"  /* row 전체가 edit 모드로 된다. 컬럼 더블클릭시 actions가 바뀐다. */
+          rowModesModel={rowModesModel}
+          onRowModesModelChange={handleRowModesModelChange}
+          onRowEditStop={handleRowEditStop}
+          processRowUpdate={processRowUpdate}
+        />
+      </Box>
+
+      <Box sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        mt: 1,
+      }}>
+        <Stack direction="row" alignItems="center">
+          <Select
+            size="small"
+            value={pageSize}
+            label="size"
+            onChange={handleSize}
+          >
+            <MenuItem value={5}>5</MenuItem>
+            <MenuItem value={10}>10</MenuItem>
+            <MenuItem value={20}>20</MenuItem>
+            <MenuItem value={50}>50</MenuItem>
+          </Select>
+          <Typography variant="body1" sx={{ml: 2}}>total: {total}</Typography>
+        </Stack>
+        <Pagination count={count} page={page} onChange={handleChangePage} />
+      </Box>
 
       <section>
         <Typography variant="h5" sx={{fontWeight: 'bold'}}>Add News</Typography>
